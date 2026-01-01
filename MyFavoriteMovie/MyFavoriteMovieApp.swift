29fa83167f8cc4e9c988377e22839e652b1e4234
Mattis44 <mattisnaud@outlook.fr>
2026-01-01 13:11:35 +0100

    Initial Commit

---
+++ MyFavoriteMovie/MyFavoriteMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteMovieApp.swift
+//  MyFavoriteMovie
+//
+//  Created by Mattis Naud on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
